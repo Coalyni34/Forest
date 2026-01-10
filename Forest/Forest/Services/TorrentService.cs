a1821f855fa309f2d91fbd18a7fbe1bf6f5b22a9
Coalyni34 <coalyni34work@proton.me>
2026-01-11 01:59:45 +0300

    fix

--- Forest/Forest/Services/TorrentService.cs
+++ Forest/Forest/Services/TorrentService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using MonoTorrent;
 using MonoTorrent.Client;
@@ -14,30 +15,5 @@ public class TorrentService
         }.ToSettings();
 
         _engine = new ClientEngine(engineSettings);
-    }
-    public async Task Test(string path, string directory)
-    {
-        var torrent = await Torrent.LoadAsync(path);
-        var manager = await _engine.AddAsync(torrent, directory);
-        manager.PeersFound += (sender, e) => 
-            Console.WriteLine($"Найдено пиров: {e.NewPeers}");
-        manager.TorrentStateChanged += (sender, e) => 
-            Console.WriteLine($"Состояние изменилось: {e.NewState}");
-            await manager.StartAsync();
-
-        Console.WriteLine($"Загрузка начата: {torrent.Name}");
-        while (manager.State != TorrentState.Stopped)
-        {
-            Console.WriteLine($"Прогресс: {manager.Progress:F2}%, " +
-                              $"Скорость: {manager.Monitor.DownloadRate / 1024:F1} КБ/с");
-
-            await Task.Delay(1000);
-
-            if (manager.Progress == 100.0)
-            {
-                Console.WriteLine("Загрузка завершена!");
-                break;
-            }
-        }
     }
 }
