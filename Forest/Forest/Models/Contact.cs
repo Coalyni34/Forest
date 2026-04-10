d45d7026f1ff03b7274d8a7b91d3cc3fa6d3f1df
Coalyni34 <coalyni34work@proton.me>
2026-04-11 00:37:50 +0300

    removed avatar

--- Forest/Forest/Models/Contact.cs
+++ Forest/Forest/Models/Contact.cs
@@ -1,18 +1,16 @@
 public class Contact
 {
     public string Name { set; get; } //Public Name
-    public string Avatar { set; get; } //Public Avatar
     public string PublicId { set; get;} //Public ID 
     public string PublicKey { set; get; } //Ed25519 Public Key
     public string EncryptionKey { set; get; } //X25519 ublic Key
     public bool isPrivate { set; get; }  //This value means that will you share your contact by DHT with other users or not
     
-    public Contact(string Name, bool isPrivate, string PublicId, string Avatar = "")
+    public Contact(string Name, bool isPrivate, string PublicId)
     {
         this.Name = Name;
         this.isPrivate = isPrivate;
         this.PublicId = PublicId;
-        this.Avatar = Avatar;
     }
     public Contact()
     {
